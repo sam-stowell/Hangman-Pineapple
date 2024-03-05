338ad1801d8bd03225554fa71ed8c208bbb49902
sam-stowell <samueljamesstowell@gmail.com>
2024-03-05 13:10:40 +0000

    Basic game start background, buttons and config

---
+++ Config.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+# Colours
+RED = (255, 129, 80)
+ORANGE = (255, 193, 57)
+YELLOW = (255, 223, 0)
+GREEN = (93, 255, 207)
+CYAN = (62, 241, 252)
+BLUE = (78, 201, 255)
+
+# Button color
+button = (255, 255, 255)
+button_light = (170, 170, 170)
+button_dark = (100, 100, 100)
+
+# Button dimensions
+button_width = 140
+button_height = 40
+
+# Define font
+button_font = pygame.font.SysFont('Corbel', 35)
---
+++ PineappleGame.py
@@ -0,0 +1,89 @@
+import pygame
+from Config import *
+pygame.init()
+pygame.font.init()
+
+# Open a new window
+size = (1920, 1080)
+screen = pygame.display.set_mode(size)
+# Toggle fullscreen --------------- dont add until minimise and close button
+# pygame.display.toggle_fullscreen()
+pygame.display.set_caption("Pineapple")
+
+# Background
+screen.fill(CYAN)
+# screen.fill(YELLOW, (0, 0, screen.get_height()// 2, screen.get_width()))
+screen.fill(YELLOW, (0, (screen.get_height()// 3) * 2, 1920, screen.get_height()// 3))
+pygame.display.flip()
+
+# Get the screen dimensions
+width = screen.get_width()
+height = screen.get_height()
+
+# Render "Quit" text
+quit_text = button_font.render('Quit', True, button)
+
+# Render "Minimize" text
+minimize_text = button_font.render('Minimize', True, button)
+
+while True:
+    # Get mouse position
+    mouse = pygame.mouse.get_pos()
+
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+            # Checks if a mouse is clicked
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            # The game is terminated
+            if width - 140 <= mouse[0] <= width and 0 <= mouse[1] <= 40:
+                pygame.quit()
+
+            # Check if the mouse is clicked on the "Minimize" button
+            elif width - 2 * button_width <= mouse[0] <= width - button_width and 0 <= mouse[1] <= button_height:
+                pygame.display.iconify()
+
+    # if mouse is hovered on a button it changes to lighter shade
+    if width - 140 <= mouse[0] <= width and 0 <= mouse[1] <= 40:
+        pygame.draw.rect(screen, button_light, [width - 140, 0, 140, 40])
+
+    else:
+        pygame.draw.rect(screen, button_dark, [width - 140, 0, 140, 40])
+
+    # Draw minimise button
+    if width - 2 * button_width <= mouse[0] <= width - button_width and 0 <= mouse[1] <= button_height:
+        pygame.draw.rect(screen, button_light, [width - 2 * button_width, 0, button_width, button_height])
+    else:
+        pygame.draw.rect(screen, button_dark, [width - 2 * button_width, 0, button_width, button_height])
+
+    # Calculate the center of the buttons
+    button_center_x_quit = width - button_width + button_width // 2
+    button_center_y = button_height // 2
+
+    button_center_x_minimize = width - 2 * button_width + button_width // 2
+
+    # Calculate the position to center the "Quit" text within the "Quit" button
+    quit_text_x = button_center_x_quit - quit_text.get_width() // 2
+    quit_text_y = button_center_y - quit_text.get_height() // 2
+
+    # Calculate the position to center the "Minimize" text within the "Minimize" button
+    minimize_text_x = button_center_x_minimize - minimize_text.get_width() // 2
+    minimize_text_y = button_center_y - minimize_text.get_height() // 2
+
+    # Superimpose "Quit" text onto the "Quit" button
+    screen.blit(quit_text, (quit_text_x, quit_text_y))
+
+    # Superimpose "Minimize" text onto the "Minimize" button
+    screen.blit(minimize_text, (minimize_text_x, minimize_text_y))
+
+    # Updates the frames of the game
+    pygame.display.update()
+
+
+# Close the window
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
